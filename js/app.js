8f87e5064d59d77bc1a68949f8097a150e1e7388
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-09-13 16:54:36 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+      "domanda": "Qual è la capitale dell'Italia?",
+      "risposte": [
+        {"text": "Roma", "correct": true},
+        {"text": "Milano", "correct": false},
+        {"text": "Napoli", "correct": false},
+        {"text": "Torino", "correct": false}
+      ]
+    },
+    {
+      "domanda": "Qual è il pianeta più vicino al Sole?",
+      "risposte": [
+        {"text": "Mercurio", "correct": true},
+        {"text": "Venere", "correct": false},
+        {"text": "Terra", "correct": false},
+        {"text": "Marte", "correct": false}
+      ]
+    },
+    {
+      "domanda": "Chi ha scritto 'La Divina Commedia'?",
+      "risposte": [
+        {"text": "Dante Alighieri", "correct": true},
+        {"text": "Giovanni Boccaccio", "correct": false},
+        {"text": "Francesco Petrarca", "correct": false},
+        {"text": "Ludovico Ariosto", "correct": false}
+      ]
+    },
+    {
+      "domanda": "Quale gas compone principalmente l'atmosfera terrestre?",
+      "risposte": [
+        {"text": "Azoto", "correct": true},
+        {"text": "Ossigeno", "correct": false},
+        {"text": "Anidride carbonica", "correct": false},
+        {"text": "Idrogeno", "correct": false}
+      ]
+    }
+  ]
+  
